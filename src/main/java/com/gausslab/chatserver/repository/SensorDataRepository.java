8744ea828a35c9bade7585e5926f5048dd7ec88a
Changmin Bae <65561206+baec23@users.noreply.github.com>
2022-07-08 14:29:36 +0900

    cleanup

--- src/main/java/com/gausslab/chatserver/repository/SensorDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gausslab.chatserver.repository;
-
-import com.gausslab.chatserver.model.SensorData;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface SensorDataRepository extends MongoRepository<SensorData, String>
-{
-
-}
